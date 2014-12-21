1bf92260ab9aba651230b2dc502bf38d10e4a9af
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-21 02:39:56 +0000

    Add menu button

--- src/main.elm
+++ src/main.elm
@@ -7,16 +7,26 @@ import Mouse
 import Window
 
 blue = Color.rgb 0x0C 0xFE 0xFF
-
-leftMargin = spacer 20 20
-topMargin  = spacer 20 20
+spacer' x = spacer (round x) (round x)
+margin x = x/30
 
 logo w h color =
     let aspect = 3.394144559879574
-    in  fittedImage (floor (w / 2)) (floor (w / 2 / aspect))
-       ("images/logo-" ++ color ++ ".svg")
+    in scaledImage w h 2 aspect 100 350 ("images/logo-" ++ color ++ ".svg")
 
-scene (w',h') = let (w,h) = (toFloat w', toFloat h')
-                in flow down [topMargin, flow right [leftMargin, logo w h "red"]]
+withMargins w h x = flow down [spacer' (margin w), flow right [spacer' (margin h), x]]
 
-main = scene <~ Window.dimensions
+scaledImage w h scale aspect min max path =
+    fittedImage (floor (clamp min max (w / scale))) (floor (clamp (min / aspect) (max / aspect) (w / scale / aspect))) path
+
+menuButton w h =
+    let aspect = 1.548105123408364
+    in scaledImage w h 10 aspect 25 50 "images/menu_button.svg"
+
+scene (w',h') connected =
+    let (w,h) = (toFloat w', toFloat h')
+    in  case connected of
+        False -> withMargins w h <| logo w h "red"
+        True  -> withMargins w h <| flow right [logo w h "blue", spacer' (w/4), menuButton w h]
+
+main = scene <~ Window.dimensions ~ constant True
