8623cbd52f39f2332b535963f782707cae0e4372
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-21 04:40:48 +0000

    Add type signatures

--- src/main.elm
+++ src/main.elm
@@ -26,10 +26,12 @@ app : (Int, Int) -> Bool -> Element
 app dims connected = let state = {defaultState | connected <- connected}
                      in  scene dims state
 
+scene : (Int, Int) -> State -> Element
 scene (w',h') state =
     let (w,h) = (toFloat w', toFloat h')
     in withMargins w h <| header (w - (2*(margin w))) state.connected
 
+header : Float -> Bool -> Element
 header w connected =
     if connected
     then
@@ -39,20 +41,28 @@ header w connected =
     else
         logo w "red"
 
+logo : Float -> String -> Element
 logo w color =
     let aspect = 3.394144559879574
         logo'  = image (round (85 * aspect)) 85 ("images/logo-" ++ color ++ ".svg")
     in  container (max 300 (round (w * 0.749))) 85 midLeft logo'
 
+menuButton : Float -> Element
 menuButton w =
     let aspect = 1.548105123408364
         button = image (round (32 * aspect)) 32 ("images/menu_button.svg")
     in  container (max 85 (round (w * 0.249))) 85 middle button
 
+blue : Color.Color
 blue = Color.rgb 0x0C 0xFE 0xFF
+
+spacer' : Float -> Element
 spacer' x = spacer (round x) (round x)
+
+margin : Float -> Float
 margin x = x/20
 
+withMargins : Float -> Float -> Element -> Element
 withMargins w h x = flow down [spacer' (margin h), flow right [spacer' (margin w), x, spacer' (margin w)], spacer' (margin h)]
 
 
