1319e7e2248b4dc2b2dcb29db24944cb3e208b94
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-21 01:24:39 +0000

    Add logo images

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
-all:
-	mkdir -p build
+all: build/.dirstamp images
 	elm-make src/main.elm --output build/elm.js
 	install src/index.html build/
+
+build/.dirstamp:
+	mkdir -p build
+	touch build/.dirstamp
+
+images: build/.dirstamp
+	mkdir -p build
+	cp -r src/images build/
+
+.PHONY: all images
--- src/main.elm
+++ src/main.elm
@@ -1,12 +1,22 @@
 import Text(..)
 import Graphics.Element(..)
 import Graphics.Collage(..)
-import Color(..)
+import Color
 import Signal(..)
 import Mouse
 import Window
 
-background (w',h') = let (w,h) = (toFloat w', toFloat h')
-                     in collage w' h' [filled green <| square 64]
+blue = Color.rgb 0x0C 0xFE 0xFF
 
-main = background <~ Window.dimensions
+leftMargin = spacer 20 20
+topMargin  = spacer 20 20
+
+logo w h color =
+    let aspect = 3.394144559879574
+    in  fittedImage (floor (w / 2)) (floor (w / 2 / aspect))
+       ("images/logo-" ++ color ++ ".svg")
+
+scene (w',h') = let (w,h) = (toFloat w', toFloat h')
+                in flow down [topMargin, flow right [leftMargin, logo w h "red"]]
+
+main = scene <~ Window.dimensions
