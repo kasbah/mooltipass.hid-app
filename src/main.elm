a30975fc6364a4aa4837a35f8db2c812c75053d7
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-21 04:27:50 +0000

    Add state type

--- src/main.elm
+++ src/main.elm
@@ -6,29 +6,48 @@ import Signal(..)
 import Mouse
 import Window
 
-main = scene <~ Window.dimensions ~ foldp (\_ s -> not s) False Mouse.clicks
+type Screen = Log | Settings | Manage | Developer
 
-scene (w',h') connected =
+type alias State =
+    { connected : Bool
+    , screen    : Screen
+    }
+
+defaultState : State
+defaultState =
+    { connected = False
+    , screen    = Log
+    }
+
+main : Signal Element
+main = app <~ Window.dimensions ~ foldp (\_ s -> not s) False Mouse.clicks
+
+app : (Int, Int) -> Bool -> Element
+app dims connected = let state = {defaultState | connected <- connected}
+                     in  scene dims state
+
+scene (w',h') state =
     let (w,h) = (toFloat w', toFloat h')
-    in withMargins w h <| header (w - (2*(margin w))) (h - (2*(margin h))) connected
+    in withMargins w h <| header (w - (2*(margin w))) state.connected
 
-header w h connected =
-    if connected then
+header w connected =
+    if connected
+    then
         flow right [ logo w "blue"
                    , menuButton w
                    ]
-                 else
+    else
         logo w "red"
 
 logo w color =
     let aspect = 3.394144559879574
         logo'  = image (round (85 * aspect)) 85 ("images/logo-" ++ color ++ ".svg")
-    in  container (round (w/1.5)) 85 midLeft logo'
+    in  container (max 300 (round (w * 0.749))) 85 midLeft logo'
 
 menuButton w =
     let aspect = 1.548105123408364
         button = image (round (32 * aspect)) 32 ("images/menu_button.svg")
-    in  container (round (w/2.5)) 85 middle button
+    in  container (max 85 (round (w * 0.249))) 85 middle button
 
 blue = Color.rgb 0x0C 0xFE 0xFF
 spacer' x = spacer (round x) (round x)
