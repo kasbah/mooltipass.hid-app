230b270e6093dea7ebc3552ac4093a91e7c3e5c4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-21 06:58:04 +0000

    Change build to loadable chrome-app

--- Makefile
+++ Makefile
@@ -1,13 +1,24 @@
-all: build/.dirstamp images
+
+all: dir images build/manifest.json build/main.js
 	elm-make src/main.elm --output build/elm.js
 	install src/index.html build/
+
+js: $(patsubst src/%, build/%, $(wildcard src/*.js))
+
+build/%.js: src/%.js
+	install $< $@
+
+dir: build/.dirstamp
 
 build/.dirstamp:
 	mkdir -p build
 	touch build/.dirstamp
 
-images: build/.dirstamp
+build/manifest.json: dir
+	install src/manifest.json build/
+
+images: dir
 	mkdir -p build
 	cp -r src/images build/
 
-.PHONY: all images
+.PHONY: all images dir js
--- src/main.elm
+++ src/main.elm
@@ -32,24 +32,26 @@ scene (w',h') state =
     let (w,h)        = (toFloat w', toFloat h')
         withMargin x = x - 2*(margin x)
         header'      = header (withMargin w) state.connected
-        console'     = console (withMargin w) ((withMargin h) - (85 + 32 + 64))
+        console'     = console (withMargin w) ((withMargin h) - (117 + 64))
+        connectButton' = connectButton (withMargin w) ((withMargin h) - (117 + 64))
     in if not state.connected
-       then withMargins w h <| header'
+       then withMargins w h <| flow down [ header'
+                                         , connectButton'
+                                         ]
        else withMargins w h <| flow down [ header'
-                                         , spacer' 32
                                          , console'
                                          , clearButton (withMargin w)
                                          ]
 
 header : Float -> Bool -> Element
 header w connected =
-    if connected
-    then
-        flow right [ logo w "blue"
-                   , menuButton w
-                   ]
-    else
-        logo w "red"
+    let header' = if connected
+                  then flow right [ logo w "blue"
+                                  , menuButton w
+                                  ]
+                  else flow right [ logo w "red"
+                                  ]
+    in container (round w) 117 midLeft header'
 
 console : Float -> Float -> Element
 console w h = collage (round w) (round h) [filled grey <| roundedRect w h (max w h/80)]
@@ -70,7 +72,13 @@ clearButton : Float -> Element
 clearButton w =
     let aspect = 2.96658357613427
         button = image (round (32 * aspect)) 32 ("images/clear_button.svg")
-    in  container (round w) 64 middle button
+    in  container (round w) 48 midBottom button
+
+connectButton : Float -> Float -> Element
+connectButton w h =
+    let aspect = 3.8123121543185907
+        button = image (round (32 * aspect)) 32 ("images/connect_button.svg")
+    in  container (round w) (round h) middle button
 
 blue : Color.Color
 blue = Color.rgb 0x0C 0xFE 0xFF
@@ -82,7 +90,7 @@ spacer' : Float -> Element
 spacer' x = spacer (round x) (round x)
 
 margin : Float -> Float
-margin x = min 40 (x/20)
+margin x = min 40 (x/40)
 
 withMargins : Float -> Float -> Element -> Element
 withMargins w h x = flow down [spacer' (margin h), flow right [spacer' (margin w), x, spacer' (margin w)], spacer' (margin h)]
@@ -95,7 +103,7 @@ arc (cx, cy) (a, b) (startAngle, endAngle) =
       f i = (cx + a * cos (t*i + startAngle), cy + b * sin (t*i + startAngle))
   in List.map f [0..n-1]
 
-{-| A rounded rectangle with a given width, height and corner radius. -}
+{-| A rounded rec tangle with a given width, height and corner radius. -}
 roundedRect : Float -> Float -> Float -> Shape
 roundedRect w h r =
   let hw = w/2
