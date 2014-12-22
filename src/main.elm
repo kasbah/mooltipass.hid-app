1d0ab506f64c4996259b0ef744a0b28bee81f316
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-22 00:33:45 +0000

    Reduce size of header

--- src/main.elm
+++ src/main.elm
@@ -29,11 +29,11 @@ app dims connected = let state = {defaultState | connected <- connected}
 
 scene : (Int, Int) -> State -> Element
 scene (w',h') state =
-    let (w,h)        = (toFloat w', toFloat h')
-        withMargin x = x - 2*(margin x)
-        header'      = header (withMargin w) state.connected
-        console'     = console (withMargin w) ((withMargin h) - (117 + 64))
-        connectButton' = connectButton (withMargin w) ((withMargin h) - (117 + 64))
+    let (w,h)          = (toFloat w', toFloat h')
+        withMargin x   = x - 2*(margin x)
+        header'        = header (withMargin w) state.connected
+        console'       = console (withMargin w) ((withMargin h) - (64 + 64))
+        connectButton' = connectButton (withMargin w) ((withMargin h) - (64 + 64))
     in if not state.connected
        then withMargins w h <| flow down [ header'
                                          , connectButton'
@@ -51,7 +51,7 @@ header w connected =
                                   ]
                   else flow right [ logo w "red"
                                   ]
-    in container (round w) 117 midLeft header'
+    in container (round w) 64 midLeft header'
 
 console : Float -> Float -> Element
 console w h = collage (round w) (round h) [filled grey <| roundedRect w h (max w h/80)]
@@ -59,25 +59,25 @@ console w h = collage (round w) (round h) [filled grey <| roundedRect w h (max w
 logo : Float -> String -> Element
 logo w color =
     let aspect = 3.394144559879574
-        logo'  = image (round (85 * aspect)) 85 ("images/logo-" ++ color ++ ".svg")
-    in  container (max 300 (round (w * 0.749))) 85 midLeft logo'
+        logo'  = image (round (48 * aspect)) 48 ("images/logo-" ++ color ++ ".svg")
+    in  container (max (round (48 * aspect)) (round (w * 0.749))) 48 midLeft logo'
 
 menuButton : Float -> Element
 menuButton w =
     let aspect = 1.548105123408364
-        button = image (round (32 * aspect)) 32 ("images/menu_button.svg")
-    in  container (max 85 (round (w * 0.249))) 85 middle button
+        button = image (round (19 * aspect)) 19 ("images/menu_button.svg")
+    in  container (max 48 (round (w * 0.249))) 48 middle button
 
 clearButton : Float -> Element
 clearButton w =
     let aspect = 2.96658357613427
-        button = image (round (32 * aspect)) 32 ("images/clear_button.svg")
+        button = image (round (24 * aspect)) 24 ("images/clear_button.svg")
     in  container (round w) 48 midBottom button
 
 connectButton : Float -> Float -> Element
 connectButton w h =
     let aspect = 3.8123121543185907
-        button = image (round (32 * aspect)) 32 ("images/connect_button.svg")
+        button = image (round (24 * aspect)) 24 ("images/connect_button.svg")
     in  container (round w) (round h) middle button
 
 blue : Color.Color
@@ -90,10 +90,10 @@ spacer' : Float -> Element
 spacer' x = spacer (round x) (round x)
 
 margin : Float -> Float
-margin x = min 40 (x/40)
+margin x = min 30 (x/60)
 
 withMargins : Float -> Float -> Element -> Element
-withMargins w h x = flow down [spacer' (margin h), flow right [spacer' (margin w), x, spacer' (margin w)], spacer' (margin h)]
+withMargins w h x = flow right [spacer' (margin w), x, spacer' (margin w)]
 
 {-| An elliptical arc with the given center, radii and angle interval. -}
 arc : (Float, Float) -> (Float, Float) -> (Float, Float) -> Shape
