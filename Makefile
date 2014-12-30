75da20380e1b065027b4458501d59c2d77faab46
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2014-12-30 13:02:21 +0000

    Refactor roundedRect into a module

--- Makefile
+++ Makefile
@@ -1,15 +1,10 @@
-all: dir images build/manifest.json js
-	elm-make src/main.elm --output build/elm.js
+ELM_FILES = $(wildcard src/*.elm)
+JS_FILES  = $(wildcard src/*.js)
+
+all: dir images build/manifest.json elm js
 	cp src/index.html build/
 
-js: $(patsubst src/%, build/%, $(wildcard src/*.js))
-
-build/%.js: src/%.js
-	cp $< $@
-
-build/.dirstamp:
-	mkdir -p build
-	touch build/.dirstamp
+dir: build/.dirstamp
 
 build/manifest.json: dir
 	cp src/manifest.json build/
@@ -17,9 +12,20 @@ build/manifest.json: dir
 images: dir
 	cp -r src/images build/
 
-dir: build/.dirstamp
+elm: build/elm.js
+js: $(patsubst src/%, build/%, $(JS_FILES))
+
+build/.dirstamp:
+	mkdir -p build
+	touch build/.dirstamp
+
+build/elm.js:
+	elm-make $(ELM_FILES) --output build/elm.js
+
+build/%.js: src/%.js
+	cp $< $@
 
 clean:
 	rm -rf build
 
-.PHONY: all images dir js clean
+.PHONY: all images dir js elm clean
--- src/main.elm
+++ src/main.elm
@@ -7,6 +7,7 @@ import Mouse
 import Signal(..)
 import Text
 import Window
+import CustomGraphics (roundedRect)
 
 main : Signal Element
 main = scene <~ Window.dimensions ~ (foldp update defaultState (subscribe actions))
@@ -140,24 +141,3 @@ margin x = min 30 (x/60)
 
 withMargins : Float -> Float -> Element -> Element
 withMargins w h x = flow right [spacer' (margin w), x, spacer' (margin w)]
-
-{-| An elliptical arc with the given center, radii and angle interval. -}
-arc : (Float, Float) -> (Float, Float) -> (Float, Float) -> Shape
-arc (cx, cy) (a, b) (startAngle, endAngle) =
-  let n = 50
-      t = (endAngle - startAngle) / n
-      f i = (cx + a * cos (t*i + startAngle), cy + b * sin (t*i + startAngle))
-  in List.map f [0..n-1]
-
-{-| A rounded rec tangle with a given width, height and corner radius. -}
-roundedRect : Float -> Float -> Float -> Shape
-roundedRect w h r =
-  let hw = w/2
-      hh = h/2
-  in (arc (0-hw+r, 0-hh+r) (r, r) (270 |> degrees, 180 |> degrees)) ++
-     (arc (0-hw+r, hh-r) (r, r) (180 |> degrees, 90 |> degrees)) ++
-     (arc (hw-r, hh-r) (r, r) (90 |> degrees, 0 |> degrees)) ++
-     (arc (hw-r, 0-hh+r) (r, r) (0 |> degrees, -90 |> degrees)) ++
-     [(0-hw+r, 0-hh)]
-
-
