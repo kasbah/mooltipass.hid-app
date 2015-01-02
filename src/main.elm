facdf1befa3b34f57ba31584bd99f92d38ef3193
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2015-01-02 12:24:15 +0000

    Work on developer tab and clear out old code

--- src/main.elm
+++ src/main.elm
@@ -10,42 +10,56 @@ import Window
 import CustomGraphics (roundedRect)
 
 main : Signal Element
-main = scene <~ Window.dimensions ~ (foldp update defaultState (subscribe actions))
+main = scene <~ Window.dimensions
+             ~ (foldp update defaultState (subscribe actions))
 
 -- State
-type Tab = Log | Settings | Manage | Develop
+type Tab = Log | Settings | Manage | Developer
 type ConnectState = NotConnected | Connected | NoCard | NoPin
 
 {-| The entire application state -}
 type alias State =
-    { connected  : ConnectState
-    , activeTab  : Tab
-    , devEnabled : Bool
-    , log        : String
+    { connect     : ConnectState
+    , activeTab   : Tab
+    , iconClicked : Int
+    , devEnabled  : Bool
+    , log         : String
     }
 
 defaultState : State
 defaultState =
-    { connected  = Connected
-    , activeTab  = Log
-    , devEnabled = True
-    , log        = "connecting ..."
+    { connect     = NotConnected
+    , activeTab   = Log
+    , iconClicked = 0
+    , devEnabled  = True
+    , log         = "connecting ..."
     }
 
 {-| All actions that can be performed to change state -}
 type Action = ChangeTab Tab
-            | SetConnected ConnectState
             | ClearLog
+            | ClickIcon
             | NoOp
+            | SetConnected ConnectState
 
 {-| Transform the state to a new state according to an action -}
 update : Action -> State -> State
-update action state =
+update action s =
     case action of
-        (ChangeTab t)    -> {state | activeTab <- t}
-        (SetConnected c) -> {state | connected <- c}
-        ClearLog         -> {state | log <- ""}
-        NoOp             -> state
+        (ChangeTab t)    -> {s | activeTab   <- t}
+        (SetConnected c) -> {s | connect     <- c}
+        ClearLog         -> {s | log         <- ""}
+        -- clicking the icon 7 times toggles developer tab visibility
+        ClickIcon        -> if s.iconClicked >= 6
+                            then { s | iconClicked <- 0
+                                     , devEnabled <- not s.devEnabled
+                                     , activeTab <- if s.activeTab == Developer
+                                                        && s.devEnabled
+                                                    then Log else s.activeTab
+                                 }
+
+                            else {s | iconClicked <- s.iconClicked + 1}
+        NoOp             -> s
 
 actions : Channel Action
 actions = channel NoOp
@@ -62,48 +76,42 @@ layer1 dims state =
               ]
 
 heights =
-    { marginTop  = 16
-    , tab     = 32
-    , icon    = 32
-    , nav     = 42
+    { marginTop      = 16
+    , tab            = 32
+    , icon           = 32
+    , nav            = 42
     , consoleButton  = 28
     , consoleToolbar = 48
     , marginBottom   = 3
     }
 
--- Title
---title : (Int, Int) -> State -> Element
---title (w,h) state =
---    let logo' = case state.connected of
---        Connected    -> logo "blue"
---        NotConnected -> logo "red"
---        NoCard       -> logo "orange"
---        NoPin        -> logo "orange"
---    in container w 64 middle logo'
---
---logo : String -> Element
---logo color =
---    let aspect = 3.394144559879574
---    in  image (round (toFloat heights.logo * aspect)) heights.logo ("images/logo-" ++ color ++ ".svg")
-
-statusIcon : String -> Element
-statusIcon color =
-    let aspect = 1.3285316308250572
-    in  image (round (toFloat heights.icon * aspect)) heights.icon ("images/status_icon-" ++ color ++ ".svg")
+statusIcon : ConnectState -> Element
+statusIcon c =
+    let aspect          = 1.3285316308250572
+        icon color      = image (round (toFloat heights.icon * aspect)) heights.icon
+                            ("images/status_icon-" ++ color ++ ".svg")
+        clickIcon color = clickable (send actions ClickIcon) (icon color)
+    in case c of
+        Connected    -> clickIcon "blue"
+        NotConnected -> clickIcon "red"
+        NoCard       -> clickIcon "orange"
+        NoPin        -> clickIcon "purple"
 
 -- Navigation
 navigation : (Int, Int) -> State -> Element
 navigation (w,h) state =
     flow right
-        [ container (round (toFloat w * 0.8)) heights.nav midLeft  (flow right [spacer 32 38, tabs state])
-        , container (round (toFloat w * 0.2)) heights.nav midRight (flow right [statusIcon "blue", spacer 32 38])
+        [ container (round (toFloat w * 0.85)) heights.nav midLeft
+            (flow right [spacer 32 38, tabs state])
+        , container (round (toFloat w * 0.15)) heights.nav midRight
+            (flow right [statusIcon state.connect, spacer 32 38])
         ]
 
 tabs : State -> Element
 tabs state =
-    let disabled = case state.connected of
+    let disabled = case state.connect of
         Connected    -> []
-        NotConnected -> [Settings, Manage, Develop]
+        NotConnected -> [Settings, Manage, Developer]
         NoCard       -> [Settings, Manage]
         NoPin        -> [Manage]
     in flow right <| [
@@ -114,16 +122,16 @@ tabs state =
                      , spacer 5 5
                      , tab Manage   state.activeTab disabled
                      , spacer 5 5
-                     ] ++ if state.devEnabled then [tab Develop state.activeTab disabled] else []
+                     ] ++ if state.devEnabled then [tab Developer state.activeTab disabled] else []
 
 tab : Tab -> Tab -> (List Tab) -> Element
 tab t active disabled =
     let aspect         = 3.094594610699232
         name = case t of
-            Log      -> "log"
-            Settings -> "settings"
-            Manage   -> "manage"
-            Develop  -> "develop"
+            Log       -> "log"
+            Settings  -> "settings"
+            Manage    -> "manage"
+            Developer -> "developer"
         img t =
             image (round (toFloat heights.tab * aspect))
                 heights.tab
@@ -131,7 +139,7 @@ tab t active disabled =
         up             = img "inactive"
         hover          = img "active"
         down           = img "active"
-        disabledButton = img "inactive"
+        disabledButton = img "disabled"
         activeButton   = img "active"
         button         = customButton (send actions (ChangeTab t)) up hover down
     in  if List.member t disabled
@@ -171,61 +179,6 @@ clearButton =
 grey : Color.Color
 grey = Color.rgb 0x1A 0x1A 0x1A
 
---connectButton : Float -> Float -> Element
---connectButton w h =
---    let aspect = 3.3747858968383753
---        up     = image (round (24 * aspect)) 24 ("images/connect_button.svg")
---        hover  = image (round (24 * aspect)) 24 ("images/connect_button_hover.svg")
---        down   = image (round (24 * aspect)) 24 ("images/connect_button_down.svg")
---        button = customButton (send actions (SetConnected True)) up hover down
---    in  container (round w) (round h) middle button
---
---popup : (Int, Int) -> Element -> State -> Element
---popup (w',h') hdr state =
---    let aspect = 0.7009962578555462
---    in  (image 200 (round (200 / aspect)) "images/popup.svg") `below` hdr
---
---header : Float -> Bool -> Element
---header w connected =
---    let header' = if connected
---                  then flow right [ logo w "blue"
---                                  , menuButton w
---                                  ]
---                  else flow right [ logo w "red"
---                                  ]
---    in container (round w) 64 midLeft header'
---
---
---
---menuChannel : Channel ()
---menuChannel = channel ()
---
---menuButton : Float -> Element
---menuButton w =
---    let aspect = 1.4404321532671787
---        up     = image (round (24 * aspect)) 24 ("images/menu_button.svg")
---        hover  = image (round (24 * aspect)) 24 ("images/menu_button_hover.svg")
---        down   = image (round (24 * aspect)) 24 ("images/menu_button_down.svg")
---        button = customButton (send clearChannel ()) up hover down
---    in  container (max 48 (round (w * 0.249))) 48 middle button
---
---clearChannel : Channel ()
---clearChannel = channel ()
---
---
---connectChannel : Channel ()
---connectChannel = channel ()
---
---
---blue : Color.Color
---blue = Color.rgb 0x0C 0xFE 0xFF
---
---
---spacer' : Float -> Element
---spacer' x = spacer (round x) (round x)
---
---margin : Float -> Float
---margin x = min 30 (x/60)
---
---withMargins : Float -> Float -> Element -> Element
---withMargins w h x = flow right [spacer' (margin w), x, spacer' (margin w)]
+blue : Color.Color
+blue = Color.rgb 0x0C 0xFE 0xFF
+
