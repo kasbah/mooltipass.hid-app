ce844d88706e296e464509726f521e61a82cb1ae
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2015-02-16 18:13:34 +0000

    Attempt function: DeviceFlash -> MemoryInfo

--- src/background/DeviceFlash.elm
+++ src/background/DeviceFlash.elm
@@ -1,18 +1,29 @@
 module DeviceFlash where
 
+import List (..)
+
 -- local source
 import Byte (..)
 import DevicePacket (..)
+import CommonState (..)
+
+type alias DeviceFlash =
+    { parentNodes : List ParentNode
+    , childNodes  : List ChildNode
+    , favorites   : List Favorite
+    }
 
 type alias ParentNode =
-    { prevParentAddress : FlashAddress
+    { address           : FlashAddress
+    , prevParentAddress : FlashAddress
     , nextParentAddress : FlashAddress
     , nextChildAddress  : FlashAddress
     , service           : ByteString
     }
 
 type alias ChildNode =
-    { nextChildAddress : FlashAddress
+    { address          : FlashAddress
+    , nextChildAddress : FlashAddress
     , prevChildAddress : FlashAddress
     , ctr              : (Byte, Byte, Byte)
     , description      : ByteString
@@ -27,3 +38,31 @@ type alias Favorite =
     , parentNode : FlashAddress
     , childNode  : FlashAddress
     }
+
+null : FlashAddress
+null = (0,0)
+
+--toInfo : DeviceFlash -> MemoryInfo
+--toInfo fl =
+--    let clearChildLess ns =
+--            foldl
+--                (\n z -> if n.nextChildAddress == null then z else n::z)
+--                []
+--                ns
+--        getService n = (n.service, n.nextChildAddress)
+--        services     = map getService (clearChildLess fl.parentNodes)
+--        getNextChild cAdd ns =
+--            if cAdd == null then Nothing
+--            else foldr
+--                (\n z -> if n.address == cAdd then Just n else z)
+--                Nothing
+--                ns
+--        getChildren firstAdd = foldr (\_ (a, matched, unmatched) -> if (head unmatched).address == a then
+--
+--    in {credentials  = map services
+--
+--type alias MemoryInfo =
+--    { credentials : List (String, List String)
+--    , favorites   : List (Maybe (String, String))
+--    }
+
