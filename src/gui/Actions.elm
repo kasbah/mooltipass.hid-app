508cbd2fecab484808142fb4b2bc971d8fddebad
Conrad Parker <conrad@metadecks.org>
2015-05-27 06:23:03 +1000

    gui.Actions: remove dead code

--- src/gui/Actions.elm
+++ src/gui/Actions.elm
@@ -22,27 +22,6 @@ commonActions = channel CommonNoOp
 sendGetParameter : Parameter -> Message
 sendGetParameter p = send guiActions (CommonAction (GetParameter (Just p)))
 
-{-
-sendParameter : Parameter -> Byte -> Message
-sendParameter p b = send guiActions (CommonAction (SetParameter (Just (p, b))))
-
-sendIntContent : Parameter -> Int -> Int -> Content -> Message
-sendIntContent p lo hi content = send guiActions (SetParameterField p lo hi content)
-
-sendParseInt : Parameter -> String -> Message
-sendParseInt p s = case toInt s of
-  Ok i -> sendParameter p i
-  _    -> send guiActions NoOp
-
-sendBool : Parameter -> Bool -> Message
-sendBool p b = case b of
-  True  -> sendParameter p 1
-  False -> sendParameter p 0
-
-setKeyboard : Int -> Message
-setKeyboard kb = sendParameter KeyboardLayout kb
--}
-
 stageParameter : Parameter -> Byte -> Message
 stageParameter p b = send guiActions (StageParameter (p, b))
 
